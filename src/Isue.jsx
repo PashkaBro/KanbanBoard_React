60833acdf198bc0a378b1ebe0a4451a7beffe289
PashkaBro <veenoompoltava@gmail.com>
2020-04-07 23:33:24 -0700

    first commit

---
+++ src/Isue.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import Card from 'react-bootstrap/Card';
+
+
+function Isue() {
+    return (
+        <div className="Isue">
+
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+
+        </div>
+    );
+}
+
+export default Isue;
